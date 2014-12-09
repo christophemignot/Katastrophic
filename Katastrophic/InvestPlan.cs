8bcc05d357848404d9124922eeae549cf1f34d9f
Christophe Mignot <christophe.mignot@axa.fr>
2014-12-09 15:25:01 +0100

    Première étape de refato détail de actions en commentaires du fichier source

---
+++ Katastrophic/InvestPlan.cs
@@ -0,0 +1,122 @@
+﻿//remplacement des types String -> string Object -> object pour respect des normes de dev
+// ==> suppression du using System
+//Ajout de tests sur Input(string input) pour découvrir la structure
+// Il y a des variables dans la structure qui ne sont pas utilisées, des noms de variable étranges et des types inadaptés
+// ==> replacement  currency ?? "€" pour currency
+// ==> renommage de variables et suppression de variables inutilisées
+// ==> suppression static et passage en instance
+//        ==> suppression du lock et du clear sur la liste : allowingProfits
+//        ==> inversion de la méthode de construction/évaluation du retour
+//        ==> expostion de la méthode input static pour encapsuler le traitement et maintenir le fonctonnement du TU
+//        ==> currency est vide, on le supprime
+// ==> correction du formatage de sortie case #%d
+// Ajout de TU
+// Renommage de variables/classe/fichier
+// Suppression des stream qui nécessite un appel à Dispose
+
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+public class InvestPlan
+{
+    public static InvestPlan Input(string input)
+    {
+        var result = new InvestPlan(input);
+        result.ProcessInvestment();
+        return result;
+    }
+
+    public const int MonthCount = 12;
+
+    private string output;
+    private readonly string input;
+    private readonly List<string> allowingProfits;
+
+    private InvestPlan(string input)
+    {
+        this.input = input;
+        allowingProfits = new List<string>();
+    }
+
+    private void ProcessInvestment()
+    {
+        IList<string> inputs = this.input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        int.Parse(inputs[0]);
+
+        for (int investmentCount = 1; investmentCount < inputs.Count; investmentCount+=2)
+        {
+            ProcessSingleInvestment(inputs[investmentCount], inputs[investmentCount+1]);
+        }
+
+        string result = string.Empty;
+        for (int i = 0; i < allowingProfits.Count; i++)
+        {
+            if (i > 0)
+            {
+                result += "\n";
+            }
+            result += string.Format("Case {0:d}: {1}", i + 1, allowingProfits[i]);
+        }
+        this.output = result.ToString(CultureInfo.InvariantCulture);
+    }
+
+    private void ProcessSingleInvestment(string amount, string pricesPerMonth)
+    {
+        int investAmount = int.Parse(amount);
+        List<int> prices = new List<int>();
+
+        // Max profit is set to min value for initialization purposes. 
+        int maxProfit = int.MinValue;
+        string[] strings = pricesPerMonth.Split(' ');
+        for (int i = 0; i < MonthCount; i++)
+        {
+            prices.Add(int.Parse(strings[i]));
+        }
+
+        // We use BigInteger for optimization on ARM processors 
+        int bestBuyMonth = int.MinValue;
+        int bestSellMonth = int.MinValue;
+
+        for (int buyMonth = 1; buyMonth <= MonthCount; buyMonth++)
+        {
+            for (int sellMonth = 1; sellMonth <= MonthCount; sellMonth++)
+            {
+                int profit = int.MinValue;
+                if (buyMonth < sellMonth)
+                {
+                    int sellPrice = prices[buyMonth - 1];
+                    int quantity = investAmount/sellPrice;
+                    profit = -quantity*sellPrice;
+                    int buyPrice = prices[sellMonth - 1];
+                    int rev = quantity*buyPrice;
+                    profit += rev;
+                }
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                    bestBuyMonth = buyMonth;
+                    bestSellMonth = sellMonth;
+                }
+            }
+        }
+
+        string bestProfit;
+        if (maxProfit <= 0)
+        {
+            bestProfit = "IMPOSSIBLE";
+        }
+        else
+        {
+            bestProfit = bestBuyMonth + " " + bestSellMonth + " " + maxProfit;
+        }
+        allowingProfits.Add(bestProfit);
+    }
+
+
+    public string Output()
+    {
+        return output;
+    }
+} 
---
+++ Katastrophic/InvestPlanTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katastrophic
+{
+    [TestClass]
+    public class InvestPlanTest
+    {
+        [TestMethod]
+        public void WhenInputIsNullOrEmptyThenThrowException()
+        {
+            try
+            {
+                var result = InvestPlan.Input(string.Empty).Output();
+            }
+            //catch (ArgumentNullException ex)
+            //{
+            //    Assert.AreEqual("String", ex.ParamName);
+            //    Assert.IsTrue(true);
+            //}
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.AreEqual("index", ex.ParamName);
+                Assert.IsTrue(true);
+            }
+
+
+            try
+            {
+                var result = InvestPlan.Input(null).Output();
+            }
+            //catch (ArgumentNullException ex)
+            //{
+            //    Assert.AreEqual("s", ex.ParamName);
+            //    Assert.IsTrue(true);
+            //}
+            catch (NullReferenceException ex)
+            {
+                Assert.IsTrue(true);
+            }  
+        }
+
+        [TestMethod]
+        public void WhenInputIsOnlyZeroThenEmpty()
+        {
+            var result = InvestPlan.Input("0").Output();
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void WhenInputIsTwoZeroThenImpossible()
+        {
+            var result = InvestPlan.Input(@"0
+0
+1 1 1 1 1 1 1 1 1 1 1 1").Output();
+            Assert.AreEqual("Case 1: IMPOSSIBLE", result);
+        }
+
+        [TestMethod]
+        public void WhenInputIsOneThenImpossible()
+        {
+            var result = InvestPlan.Input(@"0
+1
+1 1 1 1 1 1 1 1 1 1 1 1").Output();
+            Assert.AreEqual("Case 1: IMPOSSIBLE", result);
+        }
+
+        [TestMethod]
+        public void WhenInputIs10ThenEmpty()
+        {
+            var result = InvestPlan.Input(@"0
+10
+1 2 3 1 1 1 1 1 1 1 1 1").Output();
+            Assert.AreEqual("Case 1: 1 3 20", result);
+        }
+
+        [TestMethod]
+        public void WhenMutipleInputsThenMultipleResults()
+        {
+            var result = InvestPlan.Input(@"0
+10
+1 2 3 1 1 1 1 1 1 1 1 1
+20
+1 5 10 2 1 6 1 1 2 20 1 1").Output();
+            Assert.AreEqual("Case 1: 1 3 20\nCase 2: 1 10 380", result);
+        }
+
+
+
+    }
+}
